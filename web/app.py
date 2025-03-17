15f83e07461e5c71aaa435e3ac1f7283f442e888
Natalia Khodorova <icxodnik@gmail.com>
2025-03-17 06:55:27 +0100

    move socket server to the same main.py file as requested in assignment

--- web/app.py
+++ web/app.py
@@ -1,14 +1,14 @@
-
 import http.server
+import multiprocessing
 import socket
 import socketserver
 import os
 import urllib.parse
 import json
 from datetime import datetime
+import socket
 
 PORT = 3000
-SOCKET_HOST = 'message-processor'
 SOCKET_HOST = 'localhost'
 SOCKET_PORT = 5000
 STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./static")
@@ -93,5 +93,32 @@ def run_server():
         print(f"Server started at http://localhost:{PORT}")
         httpd.serve_forever()
 
+def start_socket_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 5000))
+    sock.listen(1)
+    conn, addr = sock.accept()
+
+    while True:
+        data = conn.recv(1024 * 10)
+        if not data:
+            break
+        
+        record = json.loads(data.decode('utf-8'))
+        print("Data received", record)
+        
+
+    conn.close()
+
+            
 if __name__ == "__main__":
+    
+    socket_server = multiprocessing.Process(
+        target=start_socket_server,
+        args=(),
+    )
+    socket_server.start()
+    
+    # don't need to run in separate process as we already have initial
     run_server()
